2876c9db17f88e805a16dff02ecfb748e5b22ebb
saurabhdangi03 <104308074+saurabhdangi03@users.noreply.github.com>
2024-09-25 16:41:26 +0530

    commit

--- chat-backend/index.js
+++ chat-backend/index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://chat-front-rosy.vercel.app",
         methods: ["GET", "POST"]
     }
 });
